544a4fc754cddc186785e5d59cde83fccde0b20a
Amin2906 <77432955+Amin2906@users.noreply.github.com>
2021-01-22 00:57:28 +0500

    Add files via upload

---
+++ PracticeVariables.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicevariables;
+
+/**
+ *
+ * @author MUHAMMAD AMIN
+ */
+public class PracticeVariables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 =12;
+        int num2 =8;
+        int ans= num1-num2;        
+        System.out.println("Number1"  +num1);
+        System.out.println("Number2"  +num2);
+        System.out.println("Number 1 = "+num1 + ", Number 2 = "+num2 + ", Answer = "+ans);
+    }
+    
+}
